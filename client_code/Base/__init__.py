311998f0409aae537216737c62162027b7cc8b07
aashishmankala@gmail.com <aashishmankala@gmail.com>
2023-11-06 00:14:24 +0000

    Created Base; Edited settings; Deleted Form1

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import BaseTemplate
 from anvil import *
 import anvil.server
 
-class Form1(Form1Template):
+class Base(BaseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
