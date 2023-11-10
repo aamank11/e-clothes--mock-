1a91977d3cdd588ece22f942bfea5294a8405a87
aashishmankala@gmail.com <aashishmankala@gmail.com>
2023-11-10 21:23:06 +0000

    Edited Base, Feed, MyPosts, ServerModule1 and 1 more; Deleted custom package dependencies

--- client_code/Feed/__init__.py
+++ client_code/Feed/__init__.py
@@ -9,8 +9,19 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class Feed(FeedTemplate):
+
+  feed_pic = {}
+  
   def __init__(self, **properties):
+    self.load_feed()
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
+
+  def load_feed(self):
+    user = anvil.users.get_user()
+    if user:
+      email = user ["email"]
+    else:
+      email = "default"
+    self.feed_pic['image_1'] = anvil.server.call('get_feed', email)
